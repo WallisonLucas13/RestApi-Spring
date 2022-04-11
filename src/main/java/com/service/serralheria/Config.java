701569ce03dd6d4e26cf6bae6cd33a0189fb24e5
w <lucaswalison136@gmail.com>
2022-04-11 14:37:32 -0300

    configuration PostgresSql2.0

--- src/main/java/com/service/serralheria/Config.java
+++ src/main/java/com/service/serralheria/Config.java
@@ -12,11 +12,9 @@ public class Config {
 
     @Bean
     public BasicDataSource dataSource() throws URISyntaxException {
-        URI dbUri = new URI(System.getenv("DATABASE_URL"));
-
-        String username = dbUri.getUserInfo().split(":")[0];
-        String password = dbUri.getUserInfo().split(":")[1];
-        String dbUrl = "jdbc:postgresql://" + dbUri.getHost() + ':' + dbUri.getPort() + dbUri.getPath();
+        String dbUrl = System.getenv("JDBC_DATABASE_URL");
+        String username = System.getenv("JDBC_DATABASE_USERNAME");
+        String password = System.getenv("JDBC_DATABASE_PASSWORD");
 
         BasicDataSource basicDataSource = new BasicDataSource();
         basicDataSource.setUrl(dbUrl);
